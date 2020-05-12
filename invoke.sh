510ceeeb1c600076f00d0a77547228db72fcb9a1
mgh35 <markgeorgehiggins@hotmail.com>
2020-05-11 21:56:47 -0400

    Tidy

--- invoke.sh
+++ invoke.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-curl -X GET $(cat .build/stack.json | jq -r .ServiceEndpoint)/api/unauthed
+#curl -X GET $(cat .build/stack.json | jq -r .ServiceEndpoint)/api/unauthed
 
 curl -X GET -H "Authorization: $(cat .build/tokens.json | jq -r .IdToken)" $(cat .build/stack.json | jq -r .ServiceEndpoint)/api/authed
 
--- web-login.sh
+++ web-login.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 AUTH_URL=$(cat .build/stack.json | jq -r .CognitoAuthHost)
 CLIENT_ID=$(cat .build/stack.json | jq -r .UserPoolClientId)
 
-echo "curl -X GET -v ${AUTH_URL}/login?client_id=${CLIENT_ID}&redirect_uri=http://localhost:5000/&response_type=code&scope=openid%20profile%20email&state="
+open "${AUTH_URL}/login?client_id=${CLIENT_ID}&redirect_uri=http://localhost:5000/&response_type=code&scope=openid%20profile%20email&state="
